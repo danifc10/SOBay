351b49cd496347286d99f5b65c09f8afa88522a0
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2022-11-10 12:58:56 +0000

    mod

--- promotor.c
+++ promotor.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 int main(){
+	
+	char buffer[100];
+	char categoria[30];
+	int promocao,tempo;
+	FILE *f;
+	f= fopen("ficheiro_promotores.txt","rt");
+	if(f==NULL){
+		printf("erro a ler o ficheiro");
+		return 0;
+	}
+	while(feof(f)==0){
+		fgets(buffer,100,f);
+		sscanf(buffer,"%s %d %d",categoria,&promocao,&tempo);
+	}
+	fclose(f);
+	
+
 	return 0;
 }
