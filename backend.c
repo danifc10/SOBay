0cd2690273ed23397d8ee0fc5e770cc5ae70d268
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2022-11-17 12:41:32 +0000

    mod

--- backend.c
+++ backend.c
@@ -225,7 +225,7 @@ int main()
 		printf("\n\n Deseja testar que funcionalidade?\n");
 		fgets(comando, 200, stdin);
 		aux = leComandosAdmin(comando);
-		if(outputPromotores>0){
+		if(strlen(outputPromotores)>0){
 			strcpy(outputPromotores,recebePromotor(fd_p2b));
 			printf("\nmsg:%s\n",outputPromotores);
 		}
