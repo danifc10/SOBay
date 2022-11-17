c6300004d1cd0f0fea7dd39561d29fec64b5b50f
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2022-11-17 14:08:28 +0000

    MOD

--- backend.c
+++ backend.c
@@ -235,10 +235,10 @@ int main()
 		valores.sival_int = -1;
 		if(resposta=='y'){
 			while(1){
-				
+
 				strcpy(outputPromotores,recebePromotor(fd_p2b));
 				printf("\nmsg:%s\n",outputPromotores);
-				sigqueue(pid, SIGUSR1, valores);//fechar promotor
+				sigqueue(pid, SIGUSR1, NULL);//fechar promotor
 			}
 		}
 	
