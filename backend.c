3c36d2ac052b8a67022f6345073a9ed28bbf3fee
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2022-11-19 16:41:50 +0000

    mod

--- backend.c
+++ backend.c
@@ -209,16 +209,16 @@ int loadUsersFile(char *pathname)
 
 	while (feof(f) == 0)
 	{
-		user *aux;
-		aux = malloc(sizeof(user));
-		if (!aux)
+		user aux;
+		utilizadores[j] = malloc(sizeof(user));
+		if (!utilizadores[j])
 		{
 			printf("ERRO: %s\n", getLastErrorText());
 			return -1;
 		}
 		fgets(buffer, 100, f);
-		sscanf(buffer, "%s %s %d", aux->nome, aux->password, &aux->saldo);
-		(utilizadores + j) = *aux;
+		sscanf(buffer, "%s %s %d", aux.nome, aux.password, &aux.saldo);
+		utilizadores[j] = aux;
 		j++;
 	}
 	fclose(f);
