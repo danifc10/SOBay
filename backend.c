49e268aa55041e7d4557259309362dbd934c92fb
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2022-11-19 15:54:58 +0000

    mod

--- backend.c
+++ backend.c
@@ -210,7 +210,7 @@ int loadUsersFile(char *pathname)
 	while(feof(f)==0)
 	{
 	
-		(utilizadores+j)=(user*) malloc(sizeof(user));
+		utilizadores[j]=malloc(sizeof(user));
 		if (!(utilizadores+j))
 		{
 			printf("ERRO: %s\n", getLastErrorText());
